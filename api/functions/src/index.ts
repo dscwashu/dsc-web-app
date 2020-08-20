3a8fb15a219c1597fee35c4e93ce98c0d3319138
Zachary Young <zachlyoung2022@gmail.com>
2020-08-20 17:54:26 -0400

    Remove createProfile function (deprecated)

--- api/functions/src/index.ts
+++ api/functions/src/index.ts
@@ -5,23 +5,6 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
-export const createProfile = functions.auth.user().onCreate((user) => {
-  const { email, uid } = user;
-  const domain = email?.split("@")[1];
-  if (domain === "wustl.edu") {
-    return db
-      .collection("users")
-      .doc(uid)
-      .set({ security: "internal", finishProfile: false })
-      .catch((error) => functions.logger.error(error));
-  }
-  return db
-    .collection("users")
-    .doc(uid)
-    .set({ security: "external", finishProfile: false })
-    .catch((error) => functions.logger.error(error));
-});
-
 export const deleteProfile = functions.auth.user().onDelete((user) => {
   const { uid } = user;
   return db.collection("users").doc(uid).delete();
